e13cf8b502478272480c84f476927661363abe45
Vishal Sharma <fvishalsharm123@gmail.com>
2024-06-07 17:24:18 +0530

    Navbar + Accomdation Section

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,19 @@
-
+import React from 'react';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './Pages/Home';
+import Navbar from './Component/Home/Navbar';
 
 function App() {
   return (
     <div className="App">
- hello
- 
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Navbar from '../Component/Home/Navbar'
+import AccomationSection from '../Component/Home/AccomationSection'
 
 const Home = () => {
   return (
     <div>
-      
+      {/* <Navbar/> */}
+      <AccomationSection/>
     </div>
   )
 }
